820115fde23b636a2ce40ac354c8edf0c054283a
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-17 17:30:25 +0000

    working on the create and update for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
-  def userList
+  def index
+    @users = User.all
+
     render({template: "user_displays/index"})
+  end
+
+  def show
+    @user = User.where({:id => "path_id"})
+
+    render({template: "user_displays/show"})
+  end
+
+  def create
+    @user = User.where({:id => "path_id"})
+    
+    render({template: "user_displays/show"})
+  end
+
+  def update
+    @users = User.all
+    
+    render({template: "user_displays/show"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  get("/", {controller: "users" , action: "userList"})
+  get("/", {controller: "users" , action: "index"})
+  get("/users", {controller: "users" , action: "index"})
+  get("/users/:path_username", {controller: "users" , action: "show"})
+
+  get("/create_user", {controller: "users" , action: "create"})
+  get("/update_user", {controller: "users" , action: "update"})
 end
