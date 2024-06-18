d586ba75991461abdb4f0c9aad0ede3eeb912ada
MayaS111 <mayasheriff2018@gmail.com>
2024-06-18 12:20:07 -0500

    started photos

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get("/", {controller: "users" , action: "index"})
   get("/users", {controller: "users" , action: "index"})
   get("/users/:path_username", {controller: "users" , action: "show"})
-
   #post("/insert_user", {controller: "users" , action: ""})
   post("/insert_user", {controller: "users" , action: "create"})
   get("/modify_user/:path_username", {controller: "users" , action: "update"})
   #get("/destroy_user/:path_username", {controller: "users" , action: "destroy"})
+
+  get("/photos", {controller: "photos" , action: "index"})
+  get("/photos/:path_id", {controller: "photos" , action: "show"})
 end
